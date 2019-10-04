5bdaf125825c17f545d509c54d4ec8e0d5d30e47
Rene Laverdure <renelaverdure@gmail.com>
2019-10-04 19:22:07 +1000

    changed routes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
              
               <Route path='/rooms/' exact component={Rooms} />
               <Route path="/rooms/:slug" exact component={SingleRoom} />
-              <Route path='/' exact component={Home} />
+              <Route path='/' component={Home} />
               <Route component={Error} />
           </Switch>
 
